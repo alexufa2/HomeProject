73a624a1f862eacbea5511cc69108795b9253dd4
alazarev1988 <alazarev1988@gmail.com>
2026-01-20 19:24:50 +0500

    Клиент для работы с RAbbitMq

---
+++ RabbitMq/RabbitMqCustomClient/RabbitMqCustomClient/ObjectToBytesConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace RabbitMqCustomClient
+{
+    internal class ObjectToBytesConverter
+    {
+        public static byte[] ObjectToBytes<T>(T obj)
+        {
+            if (obj == null)
+                return null;
+
+            var json = JsonConvert.SerializeObject(obj);
+            return Encoding.UTF8.GetBytes(json);
+        }
+
+        public static T BytesToObject<T>(byte[] bytes)
+        {
+            if (bytes == null)
+                return default(T);
+
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
---
+++ RabbitMq/RabbitMqCustomClient/RabbitMqCustomClient/RabbitMQSender.cs
@@ -0,0 +1,51 @@
+﻿namespace RabbitMqCustomClient
+{
+    using RabbitMQ.Client;
+    using System.Text;
+
+    public class RabbitMQSender
+    {
+        private string _hostName;
+        private int _port;
+        private string _userName;
+        private string _password;
+
+        public RabbitMQSender(string hostName, int port, string userName, string password)
+        {
+            _hostName = hostName;
+            _port = port;
+            _userName = userName;
+            _password = password;
+        }
+
+        public async Task SendMessage<T>(T message)
+        {
+            // Создаем фабрику подключения
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostName, 
+                Port = _port,            
+                UserName = _userName,     
+                Password = _password      
+            };
+
+            // Устанавливаем соединение
+            using (var connection = await factory.CreateConnectionAsync())
+            {
+                using (var channel = await connection.CreateChannelAsync())
+                {
+                    // Преобразуем сообщение в байты
+                    var body = ObjectToBytesConverter.ObjectToBytes(message);
+
+                    // Публикуем сообщение
+                    await channel.BasicPublishAsync(
+                        "",        // используем exchange по умолчанию
+                        "hello", // ключ маршрутизации (имя очереди)
+                        true,
+                        body
+                    );
+                }
+            }
+        }
+    }
+}
